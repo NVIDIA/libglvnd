60331f22d2ccafdfc2345622f1c548b7305653ce
Kyle Brenneman <kbrenneman@nvidia.com>
2017-11-29 14:38:50 -0700

    Add support for GLX_EXT_no_config_context.
    Added a dispatch handler for glXCreateContextAttribsARB. It'll look for a
    GLX_SCREEN attribute first if the caller provides one, and then the GLXFBConfig
    handle if the caller doesn't.

--- src/GLX/libglx.c
+++ src/GLX/libglx.c
@@ -262,6 +262,48 @@ PUBLIC GLXContext glXCreateNewContext(Display *dpy, GLXFBConfig config,
     return context;
 }
 
+static GLXContext glXCreateContextAttribsARB(Display *dpy, GLXFBConfig config,
+        GLXContext share_list, Bool direct, const int *attrib_list)
+{
+    GLXContext context = NULL;
+    __GLXvendorInfo *vendor = NULL;
+
+    if (attrib_list != NULL) {
+        // See if the caller passed in a GLX_SCREEN attribute, and if so, use
+        // that to select a vendor library. This is needed for
+        // GLX_EXT_no_config_context, where we won't have a GLXFBConfig handle.
+        int i;
+        for (i=0; attrib_list[i] != None; i += 2) {
+            if (attrib_list[i] == GLX_SCREEN) {
+                int screen = attrib_list[i + 1];
+                vendor = __glXGetDynDispatch(dpy, screen);
+                if (vendor == NULL) {
+                    __glXSendError(dpy, BadValue, 0,
+                            X_GLXCreateContextAtrribsARB, True);
+                    return None;
+                }
+            }
+        }
+    }
+
+    if (vendor == NULL) {
+        // We didn't get a GLX_SCREEN attribute, so look at the config instead.
+        vendor = CommonDispatchFBConfig(dpy, config, X_GLXCreateContextAtrribsARB);
+    }
+
+    if (vendor != NULL && vendor->staticDispatch.createContextAttribsARB != NULL) {
+        context = vendor->staticDispatch.createContextAttribsARB(dpy, config, share_list, direct, attrib_list);
+        if (context != NULL) {
+            if (__glXAddVendorContextMapping(dpy, context, vendor) != 0) {
+                vendor->staticDispatch.destroyContext(dpy, context);
+                context = NULL;
+            }
+        }
+    }
+
+    return context;
+}
+
 PUBLIC void glXDestroyContext(Display *dpy, GLXContext context)
 {
     __GLXvendorInfo *vendor;
@@ -1680,6 +1722,7 @@ const __GLXlocalDispatchFunction LOCAL_GLX_DISPATCH_FUNCTIONS[] =
 
         LOCAL_FUNC_TABLE_ENTRY(glXImportContextEXT)
         LOCAL_FUNC_TABLE_ENTRY(glXFreeContextEXT)
+        LOCAL_FUNC_TABLE_ENTRY(glXCreateContextAttribsARB)
 #undef LOCAL_FUNC_TABLE_ENTRY
     { NULL, NULL }
 };
