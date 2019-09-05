596b51708dedfe81358f23a9c629604bb51956c7
Kyle Brenneman <kbrenneman@nvidia.com>
2019-09-05 13:13:30 -0600

    tests: Make the glXMakeCurrent tests work without assembly support.
    Change glMakeCurrentTestResults to a GLX function instead of a GL function, so
    that it can be dispatched using a vendor-provided stub instead of having to
    generate one.
    
    Remove the testglxmclate test, and the --late option in testglxmakecurrent,
    since that only makes sense when MakeCurrentTestResults was a GL function.

--- tests/dummy/GLX_dummy.h
+++ tests/dummy/GLX_dummy.h
@@ -49,11 +49,6 @@ enum {
     GL_MC_FUNCTION_COUNTS,
 
     /*
-     * Returns a NULL-terminated string describing the name of this vendor.
-     */
-    GL_MC_VENDOR_STRING,
-
-    /*
      * Last request. Always returns NULL.
      */
     GL_MC_LAST_REQ
@@ -76,7 +71,7 @@ enum {
 typedef void (* PFNGLXEXAMPLEEXTENSIONFUNCTION) (Display *dpy, int screen, int *retval);
 
 /*
- * glMakeCurrentTestResults(): perform queries on vendor library state.
+ * glXMakeCurrentTestResults(): perform queries on vendor library state.
  *
  * This explicitly is designed to not return anything, in case a bug causes the
  * API library to dispatch this to a no-op stub. If this function returned a
@@ -93,7 +88,7 @@ typedef void (* PFNGLXEXAMPLEEXTENSIONFUNCTION) (Display *dpy, int screen, int *
  * set to NULL if there was an error, or a pointer to request-specific data
  * otherwise. The pointer may be passed into free(3).
  */
-typedef void (*PFNGLMAKECURRENTTESTRESULTSPROC)(
+typedef void (*PFNGLXMAKECURRENTTESTRESULTSPROC)(
     GLint req,
     GLboolean *saw,
     void **ret
