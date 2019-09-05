6c07451bff5e18690a11c432010bc2c7c4c62315
Kyle Brenneman <kbrenneman@nvidia.com>
2019-09-05 12:17:08 -0600

    tests: Rework testglxgetprocaddress.
    It now calls glXGetClientString up front to load the dummy vendor library. The
    GLX entrypoint generation path has its own test now, so this test can now work
    on builds without assembly support.
    
    It now uses glXQueryServerString to test calling a core GLX function. That's a
    better test because it returns a predictable value, so we can make sure that it
    actually gets dispatched to the vendor library, instead of just checking that
    it doesn't crash.
    
    For testing looking up an OpenGL function, it'll call glXGetProcAddress and
    directly call __glDispatchGetProcAddress, and make sure that it gets the same
    pointer from both. It doesn't try to call the function, since OpenGL
    dispatching has its own tests.

--- tests/testglxgetprocaddress.c
+++ tests/testglxgetprocaddress.c
@@ -27,42 +27,28 @@
  * MATERIALS OR THE USE OR OTHER DEALINGS IN THE MATERIALS.
  */
 
-#include <GL/glx.h>
-#include <GL/gl.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef __GLXextFuncPtr (* pfn_glXGetProcAddress) (const GLubyte *procName);
-static pfn_glXGetProcAddress ptr_glXGetProcAddress;
+#include <GL/glx.h>
+#include <GL/gl.h>
 
-typedef void (* pfn_glXWaitGL) (void);
-static pfn_glXWaitGL ptr_glXWaitGL;
+#include <GLdispatch.h>
+#include "dummy/GLX_dummy.h"
 
-typedef void (* pfn_glVertex3fv) (const GLfloat *v);
-static pfn_glVertex3fv ptr_glVertex3fv;
-
-typedef void (* pfn_glXExampleExtensionFunction) (Display *dpy, int screen, int *retval);
-static pfn_glXExampleExtensionFunction ptr_glXExampleExtensionFunction;
-
-typedef void (* pfn_glBogusFunc1) (int a, int b, int c);
-static pfn_glBogusFunc1 ptr_glBogusFunc1;
-
-typedef void (* pfn_glBogusFunc2) (int a, int b, int c);
-static pfn_glBogusFunc2 ptr_glBogusFunc2;
+typedef const char * (* pfn_glXQueryServerString) (Display *dpy, int screen, int name);
 
 static void *LoadFunction(const char *name);
 
 int main(int argc, char **argv)
 {
-    Display *dpy = NULL;
-    int retval = 0;
+    pfn_glXQueryServerString ptr_glXQueryServerString;
+    PFNGLXEXAMPLEEXTENSIONFUNCTION ptr_glXExampleExtensionFunction;
 
-    /*
-     * Try GetProcAddress on different classes of API functions, and bogus
-     * functions. The API library should return entry point addresses for
-     * any function beginning with gl*(), though bogus functions will resolve
-     * to no-ops.
-     */
+    Display *dpy = NULL;
+    const char *str;
+    __GLdispatchProc dispatchPtr, glxPtr;
+    int retval = 0;
 
     dpy = XOpenDisplay(NULL);
     if (dpy == NULL) {
@@ -70,61 +56,44 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    /*
-     * Load the function addresses up front. Note that in order to test GLX
-     * entrypoint generation, we have to load the functions early, before
-     * libGLX.so loads the vendor library.
-     */
-    ptr_glXGetProcAddress = (pfn_glXGetProcAddress)
-        LoadFunction("glXGetProcAddress");
-    ptr_glXWaitGL = (pfn_glXWaitGL) LoadFunction("glXWaitGL");
-    ptr_glXExampleExtensionFunction = (pfn_glXExampleExtensionFunction)
-        LoadFunction("glXExampleExtensionFunction");
-    ptr_glVertex3fv = (pfn_glVertex3fv) LoadFunction("glVertex3fv");
-    ptr_glBogusFunc1 = (pfn_glBogusFunc1) LoadFunction("glBogusFunc1");
-    ptr_glBogusFunc2 = (pfn_glBogusFunc2) LoadFunction("glBogusFunc2");
-
-    /*
-     * Test core GLX dispatch functions implemented by API library. This
-     * simply returns the symbol exported by libGLX.
-     */
-    ptr_glXGetProcAddress((const GLubyte *) "glBogusFunc1");
-    ptr_glXWaitGL();
-
     // Call glXGetClientString to force libGLX to load the vendor library.
     glXGetClientString(dpy, GLX_EXTENSIONS);
 
-    /*
-     * Test a "GLX extension" function with a vendor-neutral dispatcher
-     * implemented by the vendor library (in this case, libGLX_dummy).  If we
-     * successfully used libGLX_dummy's dispatcher, retval should be non-zero
-     * (a zero value indicates we might be calling into a no-op stub generated
-     * by libGLdispatch).
-     */
-    ptr_glXExampleExtensionFunction(dpy, 0, &retval);
-    if (!retval) {
-        printf("Unexpected glXExampleExtensionFunction() return value!\n");
+    // Test a core GLX function first.
+    ptr_glXQueryServerString = (pfn_glXQueryServerString) LoadFunction("glXQueryServerString");
+    str = ptr_glXQueryServerString(dpy, 0, GLX_VENDOR);
+    if (str == NULL) {
+        printf("glXQueryServerString returned NULL\n");
+        return 1;
+    }
+    if (strcmp(str, "testlib") != 0) {
+        printf("glXQueryServerString returned unexpected value: %s\n", str);
         return 1;
     }
 
     /*
-     * Try getting the address of the core GL function glVertex3fv().
-     * This retrieves a static stub from glapi.
-     * Note calling this function with a NULL pointer is fine since this is a
-     * no-op function while there is no context current.
+     * Test a "GLX extension" function with a vendor-neutral dispatcher
+     * implemented by the vendor library (in this case, libGLX_dummy). If we
+     * successfully used libGLX_dummy's dispatcher, retval should be 1.
      */
-    ptr_glVertex3fv(NULL);
+    ptr_glXExampleExtensionFunction = (PFNGLXEXAMPLEEXTENSIONFUNCTION)
+        LoadFunction("glXExampleExtensionFunction");
+    ptr_glXExampleExtensionFunction(dpy, 0, &retval);
+    if (retval != 1) {
+        printf("Unexpected glXExampleExtensionFunction() return value: %d\n", retval);
+        return 1;
+    }
 
-    /*
-     * These are bogus functions, but will get a valid entry point since they
-     * are prefixed with "gl". The first GetProcAddress() will early out since
-     * there should be a cached copy from the explicit call made above, but
-     * the second one will go through glapi's dynamic stub generation path.
-     *
-     * Again, calling these functions should be a no-op.
-     */
-    ptr_glBogusFunc1(0, 0, 0);
-    ptr_glBogusFunc2(1, 1, 1);
+    // Test loading a normal GL function. Load the function through
+    // glXGetProcAddress, and then again directly through libGLdispatch. We
+    // should get the same poiner for both.
+    glxPtr = LoadFunction("glVertex3fv");
+    dispatchPtr = __glDispatchGetProcAddress("glVertex3fv");
+    if (dispatchPtr != glxPtr) {
+        printf("Mismatch for function glVertex3fv: GLX returned %p, GLdispatch returned %p\n",
+                glxPtr, dispatchPtr);
+        return 1;
+    }
 
     // Success!
     return 0;
@@ -132,9 +101,18 @@ int main(int argc, char **argv)
 
 static void *LoadFunction(const char *name)
 {
-    __GLXextFuncPtr func = glXGetProcAddress((const GLubyte *) name);
+    __GLXextFuncPtr func, func2;
+    func = glXGetProcAddress((const GLubyte *) name);
     if (func == NULL) {
         printf("failed to get %s!\n", name);
+        exit(1);
+    }
+
+    // Call glXGetProcAddress again to make sure that we get the same address.
+    func2 = glXGetProcAddress((const GLubyte *) name);
+    if (func != func2) {
+        printf("glXGetProcAddress returned different address for %s: %p, %p\n",
+                name, func, func2);
         exit(1);
     }
 
